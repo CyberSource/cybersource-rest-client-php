0c6463b1f70ea2daa8d99395f90216525738293b
karthikeyan kumar <karthkum@visa.com>
2021-08-04 13:05:32 +0530

    code changes to token generator

---
+++ lib/Utilities/Flex/TokenGeneratorUtility.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CyberSource\Utilities\Flex;
+
+use CyberSource\Logging\LogFactory as LogFactory;
+
+class TokenGeneratorUtility
+{
+    private static $MyLogger=null;
+    
+    /**
+     * Constructor
+     */
+    public function __construct(\CyberSource\Logging\LogConfiguration $logConfig=null)
+    {
+        if (self::$MyLogger === null) {
+            self::$MyLogger = (new LogFactory())->getLogger(\CyberSource\Utilities\Helpers\ClassHelper::getClassName(get_class()), $logConfig);
+        }
+    }
+    
+    /**
+     * Parse token for the given jwt strin 
+     */
+    public function parseToken($jwt)
+    {
+        $splitContents = explode(".",$jwt);
+        if(sizeof($splitContents) > 1)
+        {
+            $encodedString  = $splitContents[1];
+            $decodedString = base64_decode($encodedString);
+            $transientTokenModel= json_decode($decodedString);
+            // return Transient token model and make use of that model.
+            return $transientTokenModel;
+        }
+    }
+}
+
+?>
