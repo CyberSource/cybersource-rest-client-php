56c9a6b573b93227e00fa179c40bf257786d1a08
gaubansa <gaubansa@visa.com>
2024-04-23 17:57:31 +0530

    fixing the issue for encoding

--- lib/ApiClient.php
+++ lib/ApiClient.php
@@ -244,7 +244,7 @@ class ApiClient
         } elseif ((is_object($postData) or is_array($postData)) and !in_array('Content-Type: multipart/form-data', $headers, true)) { // json model
             $postData = json_encode(\CyberSource\ObjectSerializer::sanitizeForSerialization($postData));
         }
-        $resourcePath= mb_convert_encoding($resourcePath, 'UTF-8', mb_list_encodings());
+        $resourcePath= mb_convert_encoding($resourcePath, 'UTF-8', mb_detect_encoding($resourcePath));
 
         if($this->merchantConfig->getAuthenticationType() != GlobalParameter::MUTUAL_AUTH)
         {
