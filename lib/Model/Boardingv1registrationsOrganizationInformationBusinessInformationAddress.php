397a0ea2be92c4253a96b7b1af934006488a9b85
gnongsie <gnongsie@visa.com>
2024-05-24 11:04:09 +0530

    Merged with June 2024 Release

--- lib/Model/Boardingv1registrationsOrganizationInformationBusinessInformationAddress.php
+++ lib/Model/Boardingv1registrationsOrganizationInformationBusinessInformationAddress.php
@@ -194,13 +194,13 @@ class Boardingv1registrationsOrganizationInformationBusinessInformationAddress i
         if ($this->container['locality'] === null) {
             $invalid_properties[] = "'locality' can't be null";
         }
-        // if (!preg_match("/^[0-9a-zA-Z _\\-¡-￿]+$/", $this->container['locality'])) {
-        //     $invalid_properties[] = "invalid value for 'locality', must be conform to the pattern /^[0-9a-zA-Z _\\-¡-￿]+$/.";
-        // }
+        if (!preg_match("/^[0-9a-zA-Z _\\-¡-￿]+$/", $this->container['locality'])) {
+            $invalid_properties[] = "invalid value for 'locality', must be conform to the pattern /^[0-9a-zA-Z _\\-¡-￿]+$/.";
+        }
 
-        // if (!is_null($this->container['administrativeArea']) && !preg_match("/^[0-9a-zA-Z¡-￿ ]*$/", $this->container['administrativeArea'])) {
-        //     $invalid_properties[] = "invalid value for 'administrativeArea', must be conform to the pattern /^[0-9a-zA-Z¡-￿ ]*$/.";
-        // }
+        if (!is_null($this->container['administrativeArea']) && !preg_match("/^[0-9a-zA-Z¡-￿ ]*$/", $this->container['administrativeArea'])) {
+            $invalid_properties[] = "invalid value for 'administrativeArea', must be conform to the pattern /^[0-9a-zA-Z¡-￿ ]*$/.";
+        }
 
         if (!is_null($this->container['postalCode']) && !preg_match("/^[0-9a-zA-Z ]*$/", $this->container['postalCode'])) {
             $invalid_properties[] = "invalid value for 'postalCode', must be conform to the pattern /^[0-9a-zA-Z ]*$/.";
@@ -236,12 +236,12 @@ class Boardingv1registrationsOrganizationInformationBusinessInformationAddress i
         if ($this->container['locality'] === null) {
             return false;
         }
-        // if (!preg_match("/^[0-9a-zA-Z _\\-¡-￿]+$/", $this->container['locality'])) {
-        //     return false;
-        // }
-        // if (!preg_match("/^[0-9a-zA-Z¡-￿ ]*$/", $this->container['administrativeArea'])) {
-        //     return false;
-        // }
+        if (!preg_match("/^[0-9a-zA-Z _\\-¡-￿]+$/", $this->container['locality'])) {
+            return false;
+        }
+        if (!preg_match("/^[0-9a-zA-Z¡-￿ ]*$/", $this->container['administrativeArea'])) {
+            return false;
+        }
         if (!preg_match("/^[0-9a-zA-Z ]*$/", $this->container['postalCode'])) {
             return false;
         }
@@ -337,9 +337,9 @@ class Boardingv1registrationsOrganizationInformationBusinessInformationAddress i
      */
     public function setLocality($locality)
     {
-        // if ((!preg_match("/^[0-9a-zA-Z _\\-¡-￿]+$/", $locality))) {
-        //     throw new \InvalidArgumentException("invalid value for $locality when calling Boardingv1registrationsOrganizationInformationBusinessInformationAddress., must conform to the pattern /^[0-9a-zA-Z _\\-¡-￿]+$/.");
-        // }
+        if ((!preg_match("/^[0-9a-zA-Z _\\-¡-￿]+$/", $locality))) {
+            throw new \InvalidArgumentException("invalid value for $locality when calling Boardingv1registrationsOrganizationInformationBusinessInformationAddress., must conform to the pattern /^[0-9a-zA-Z _\\-¡-￿]+$/.");
+        }
         $this->container['locality'] = $locality;
 
         return $this;
@@ -361,9 +361,9 @@ class Boardingv1registrationsOrganizationInformationBusinessInformationAddress i
      */
     public function setAdministrativeArea($administrativeArea)
     {
-        // if (!is_null($administrativeArea) && (!preg_match("/^[0-9a-zA-Z¡-￿ ]*$/", $administrativeArea))) {
-        //     throw new \InvalidArgumentException("invalid value for $administrativeArea when calling Boardingv1registrationsOrganizationInformationBusinessInformationAddress., must conform to the pattern /^[0-9a-zA-Z¡-￿ ]*$/.");
-        // }
+        if (!is_null($administrativeArea) && (!preg_match("/^[0-9a-zA-Z¡-￿ ]*$/", $administrativeArea))) {
+            throw new \InvalidArgumentException("invalid value for $administrativeArea when calling Boardingv1registrationsOrganizationInformationBusinessInformationAddress., must conform to the pattern /^[0-9a-zA-Z¡-￿ ]*$/.");
+        }
         $this->container['administrativeArea'] = $administrativeArea;
 
         return $this;
