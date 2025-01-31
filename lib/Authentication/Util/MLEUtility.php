d8f3a4adab44cb56624ded2c0f09fd0ee5736972
gaubansa <gaubansa@visa.com>
2025-01-31 12:50:42 +0530

    adding MLE Utility

---
+++ lib/Authentication/Util/MLEUtility.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CyberSource\Authentication\Util;
+/*
+Purpose : MLE encrption for request body
+*/
+
+class MLEUtility
+{
+    
+    public static function checkIsMLEForAPI($merchantConfig, $isMLESupportedByCybsForApi, $operationIds) {
+        $isMLEForAPI = false;
+        //complete the function logic
+        return $isMLEForAPI;
+    }
+
+    public static function encryptRequestPayload($merchantConfig, $requestBody) {
+        // implement encrpty payload function
+        return $requestBody;
+    }
+
+
+}
+?>
