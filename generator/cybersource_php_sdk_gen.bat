33c0c844e3bd06d8b234424a2875f003323dab53
gaubansa <gaubansa@visa.com>
2025-05-14 15:48:05 +0530

    rearranging the bat file

--- generator/cybersource_php_sdk_gen.bat
+++ generator/cybersource_php_sdk_gen.bat
@@ -12,23 +12,23 @@ endlocal
 
 java -jar swagger-codegen-cli-2.4.38.jar generate -t cybersource-php-template -i cybersource-rest-spec-php.json -l php -o ../ -c cybersource-php-config.json
 
-powershell -Command "(Get-Content ..\CyberSource\lib\Api\SearchTransactionsApi.php) | ForEach-Object { $_ -replace 'selectHeaderAccept\(\[''application/json;charset=utf-8', 'selectHeaderAccept([''*/*'} | Set-Content ..\CyberSource\lib\Api\SearchTransactionsApi.php"
-
-REM renaming long file name
-
-powershell -Command "(Get-Content ..\CyberSource\lib\Api\SecureFileShareApi.php) | ForEach-Object { $_ -replace 'selectHeaderContentType\(\[''\*_\/_\*;charset=utf-8', 'selectHeaderContentType([''*/*;charset=utf-8' } | Set-Content ..\CyberSource\lib\Api\SecureFileShareApi.php"
-
-powershell -Command "(Get-Content ..\CyberSource\docs\Api\SecureFileShareApi.md) | ForEach-Object { $_ -replace '\*\*Content-Type\*\*: \*_\/_\*;charset=utf-8', '**Content-Type**: */*;charset=utf-8' } | Set-Content ..\CyberSource\docs\Api\SecureFileShareApi.md"
-
-@REM replace sdkLinks fieldName to links for supporting links field name in request/response body
-echo "starting of replacing the links keyword in PblPaymentLinksAllGet200Response.php model"
-powershell -Command "Set-Content ..\CyberSource\lib\Model\PblPaymentLinksAllGet200Response.php ((Get-Content ..\CyberSource\lib\Model\PblPaymentLinksAllGet200Response.php -Raw) -replace '''sdkLinks'' => ''sdkLinks''', '''sdkLinks'' => ''links''')"
-echo "completed the task of replacing the links keyword in PblPaymentLinksAllGet200Response.php model"
-
-
 xcopy ..\CyberSource ..\ /s /e /y
 
 rd /s /q ..\CyberSource
+
+powershell -Command "(Get-Content ..\lib\Api\SearchTransactionsApi.php) | ForEach-Object { $_ -replace 'selectHeaderAccept\(\[''application/json;charset=utf-8', 'selectHeaderAccept([''*/*'} | Set-Content ..\lib\Api\SearchTransactionsApi.php"
+
+REM renaming long file name
+
+powershell -Command "(Get-Content ..\lib\Api\SecureFileShareApi.php) | ForEach-Object { $_ -replace 'selectHeaderContentType\(\[''\*_\/_\*;charset=utf-8', 'selectHeaderContentType([''*/*;charset=utf-8' } | Set-Content ..\lib\Api\SecureFileShareApi.php"
+
+powershell -Command "(Get-Content ..\docs\Api\SecureFileShareApi.md) | ForEach-Object { $_ -replace '\*\*Content-Type\*\*: \*_\/_\*;charset=utf-8', '**Content-Type**: */*;charset=utf-8' } | Set-Content ..\docs\Api\SecureFileShareApi.md"
+
+@REM replace sdkLinks fieldName to links for supporting links field name in request/response body
+echo "starting of replacing the links keyword in PblPaymentLinksAllGet200Response.php model"
+powershell -Command "Set-Content ..\lib\Model\PblPaymentLinksAllGet200Response.php ((Get-Content ..\lib\Model\PblPaymentLinksAllGet200Response.php -Raw) -replace '''sdkLinks'' => ''sdkLinks''', '''sdkLinks'' => ''links''')"
+echo "completed the task of replacing the links keyword in PblPaymentLinksAllGet200Response.php model"
+
 
 git checkout ..\README.md
 
